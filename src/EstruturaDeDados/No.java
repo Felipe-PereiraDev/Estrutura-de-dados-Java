dca92576a4558125374b7d2a1d255f3273dabe35
Felipe <felpera41@gmail.com>
2024-06-17 22:13:20 -0400

    lista encadeada

---
+++ src/EstruturaDeDados/No.java
@@ -0,0 +1,26 @@
+package EstruturaDeDados;
+
+public class No {
+    private No proximo;
+    private String dado;
+
+
+    public No(String dado){
+        this.dado = dado;
+        this.proximo = null;
+    }
+    public No getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No proximo) {
+        this.proximo = proximo;
+    }
+    public String getDado() {
+        return dado;
+    }
+
+    public void setDado(String dado) {
+        this.dado = dado;
+    }
+}
